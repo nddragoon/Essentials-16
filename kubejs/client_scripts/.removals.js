a7cdd81fc16f1c572b63a1f9fc02501a0e100b3c
nddragoon <nddragoon@gmail.com>
2022-05-07 14:32:44 -0500

    Worldgen work

--- kubejs/client_scripts/.removals.js
+++ kubejs/client_scripts/.removals.js
@@ -23,19 +23,39 @@ onEvent('jei.hide.items', event => {
         'lumium',
         'enderium',
         'netherite',
-        'golden'
+        'golden',
+        'apatite',
+        'cinnabar',
+        'niter'
     ]
     
     removals = [
-        'chiselsandbits:block_bit'
+        'createaddition:zinc_sheet',
+        'createdeco:zinc_sheet',
+        'eidolon:lead_ore',
+        'create:copper_ore',
+        'create:zinc_ore',
+        'occultism:copper_ore',
+        'occultism:silver_ore',
+        'tconstruct:copper_ore',
+        'tconstruct:cobalt_ore',
+        'projectred-exploration:ruby_ore',
+        'projectred-exploration:sapphire_ore',
+        'projectred-exploration:peridot_ore',
+        'ars_nouveau:arcane_ore',
+        'biggerreactos:yellorite_ore'
     ]
 
     for (element of materials) {
         removals.push(`thermal:${element}_plate`)
+        removals.push(`thermal:${element}_ore`)
         removals.push(`createaddition:${element}_rod`)
         removals.push(`immersiveengineering:stick_${element}`)
         removals.push(`immersiveengineering:plate_${element}`)
         removals.push(`create:${element}_sheet`)
+        removals.push(`immersiveengineering:ore_${element}`)
+        removals.push(`projectred-exploration:${element}_ore`)
+        removals.push(`mekanism:${element}_ore`)
     }
 
     removals.forEach(element => {
