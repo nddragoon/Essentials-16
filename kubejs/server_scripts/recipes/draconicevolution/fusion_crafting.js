ba5f76840254b8b3eb7e4d1bed52a4990e20034c
nddragoon <nddragoon@gmail.com>
2022-05-17 00:07:51 -0500

    Netherite Bee

---
+++ kubejs/server_scripts/recipes/draconicevolution/fusion_crafting.js
@@ -0,0 +1,43 @@
+onEvent('recipes', event => {
+    // energy is an integer of FE
+    // core_input is a json ingredient
+    // injector_inputs is an array of items or tags
+    // tier is the minimum tier of injectors to use
+    // output is a json item
+    function fusionCrafting(energy, core_input, injector_inputs, tier, output) {
+        ingredients = []
+        injector_inputs.forEach(input => {
+            if (input.charAt() == '#') {
+                ingredients.push({tag: input.slice(1)})
+            } else {
+                ingredients.push({item: input})
+            }
+        })
+        event.custom({
+            type: 'draconicevolution:fusion_crafting',
+            result: output,
+            catalyst: core_input,
+            total_energy: energy,
+            tier: tier,
+            ingredients: ingredients
+        })
+    }
+
+    fusionCrafting(
+        10000000,
+        Item.of('resourcefulbees:bee_jar', {"Entity":"resourcefulbees:gold_bee"}).weakNBT().toJson(),
+        [
+            '#forge:storage_blocks/netherite',
+            '#forge:storage_blocks/netherite',
+            'draconicevolution:wyvern_core',
+            'draconicevolution:wyvern_core',
+            'resourcefulbees:wither_honeycomb_block',
+            'resourcefulbees:wither_honeycomb_block'
+        ],
+        'WYVERN',
+        {
+            item: 'resourcefulbees:bee_jar',
+            nbt: {"Entity":"resourcefulbees:netherite_bee"}
+        }
+    )
+})
